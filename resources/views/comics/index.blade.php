5e631b6b90210ea9fcdf151dbcbd8b493543a37d
Carmine Faella <carminefaella5@gmail.com>
2023-05-16 15:24:11 +0200

    Index fatto

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.app')
 
-@section('page-title')
+@section('page-title','Lista dei Comics')
 
 @section('content')
 
-
-
+<div class="container d-flex flex-wrap justify-content-between">
+  @foreach ($comics as $comic)
+    <div class="card" style="width: 18rem;">
+        <img src="{{$comic->thumb}}" class="card-img-top" alt="{{$comic->title}}">
+        <div class="card-body">
+        <h5 class="card-title">{{$comic->title}}</h5>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+  @endforeach
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use App\Http\Controllers\ComicController;
 |
 */
 
+Route::get('/', [ComicController::class, 'index']);
+
 Route::resource('comics',ComicController::class);
