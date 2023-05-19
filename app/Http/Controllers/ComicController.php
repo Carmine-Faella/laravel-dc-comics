7e8ce6b012e45ae6b560f573c51485c6fc8c2c4e
Carmine Faella <carminefaella5@gmail.com>
2023-05-19 14:50:49 +0200

    Aggiunta request personalizzate

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Comic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ComicController extends Controller
 {
@@ -36,18 +37,8 @@ class ComicController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
-            'title'=>'required|max:50',
-            'description'=>'nullable|max:1000',
-            'thumb'=>'required|max:255',
-            'price'=>'required|decimal:2',
-            'series'=>'required|max:100',
-            'sale_date'=>'required|date',
-            'type'=>'required|max:50'
-        ]);
-
-
         $form_data = $request->all();
+        $form_data = $this->validation($request->all());
 
         $newComic = new Comic();
 
@@ -55,7 +46,7 @@ class ComicController extends Controller
 
         $newComic->save();
 
-        return redirect()->route('comics.show', ['comic' => $newComic->id]);
+        return redirect()->route('comics.show', ['comic' => $newComic->id])->with('status', 'Card aggiunta con successo');;
 
     }
 
@@ -93,17 +84,7 @@ class ComicController extends Controller
     public function update(Request $request, Comic $comic)
     {
         //$comic = Comic::findOrFail($id);
-
-        $request->validate([
-            'title'=>'required|max:50',
-            'description'=>'nullable|max:1000',
-            'thumb'=>'required|max:255',
-            'price'=>'required',
-            'series'=>'required|max:100',
-            'sale_date'=>'required|date',
-            'type'=>'required|max:50'
-        ]);
-
+        $form_data = $this->validation($request->all());
         $form_data = $request->all();
         $comic->update($form_data);
 
@@ -122,5 +103,39 @@ class ComicController extends Controller
         $comic->delete();
 
         return redirect()->route('comics.index', ['comic' => $comic->id]);
+    }
+
+    private function validation($data) {
+
+        $validator = Validator::make(
+            $data,
+            [
+                'title'=>'required|max:50',
+                'description'=>'nullable|max:1000',
+                'thumb'=>'required|url|max:255',
+                'price'=>'required',
+                'series'=>'required|max:100',
+                'sale_date'=>'required|date',
+                'type'=>'required|max:50'
+            ],
+            [
+                'title.required' => "Titolo richiesto",
+                'title.max' => "Deve aver massimo 50 caratteri di lunghezza",
+                'description.max' => "Deve aver massimo 255 caratteri di lunghezza",
+                'thumb.required' => "L'url dell'immagine è richiesta",
+                'thumb.url' => "Deve essere un url valida (ex. https://.....)",
+                'thumb.max' => "Deve aver massimo 255 caratteri di lunghezza",
+                'price.required' => "Prezzo richiesto",
+                'series.required' => "Serie Comic richiesta",                'cooking_time.max' => "Il tempo di cottura deve essere al massimo di 10 caratteri",
+                'series.max' => "Deve aver massimo 100 caratteri di lunghezza",
+                'sale_date.required' => "Data di acquisto richiesta",                'cooking_time.max' => "Il tempo di cottura deve essere al massimo di 10 caratteri",
+                'sale_date.date' => "Deve essere un formato di data valido (ex. 2023-05-19)",
+                'type.required' => "Tipo richiesto",                'cooking_time.max' => "Il tempo di cottura deve essere al massimo di 10 caratteri",
+                'type.max' => "Deve aver massimo 50 caratteri di lunghezza",
+            ]
+        )->validate();
+
+        return $validator;
+
     }
 }
